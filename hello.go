c2ebbb1845eb2f29423a64fe7d4a5bf19a74488f
Inam ul Haq <inamshams@gmail.com>
2016-03-10 14:43:32 +0500

    commit version 2 of helloGo program

--- hello.go
+++ hello.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Printf("Hello, world.\n")
-	fmt.Println("hello boy")
-
+	fmt.Println("Hi. I'm Inam ul Haq. Its my first hello world Go language code and have first repository on Git")
+	fmt.Println("Have a nice day. Enjoy!")
 }
